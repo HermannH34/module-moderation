be8af54b16246f821fcc7ee36deceb4395cb6242
Hermann Hairet <hermannhairet44@gmail.com>
2023-01-19 20:57:12 +0100

    finish method moderation

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,13 +15,14 @@ class Message < ApplicationRecord
     uri = URI('https://moderation.logora.fr/predict')
     params = { :text => "#{comment}" }
     uri.query = URI.encode_www_form(params)
-    # res = Net::HTTP.get_response(uri)
-    res = callApi(uri)
+    res = Net::HTTP.get_response(uri)
+    # res = callApi(uri)
     prediction = JSON.parse(res.body)
-    # debugger
+
+    prediction["prediction"]["0"].round(2) >= 0.09 ? self.is_accepted = false : self.is_accepted = true
   end
 
-  def callApi(uri)
-    Net::HTTP.get_response(uri)
-  end
+  # def callApi(uri)
+  #   Net::HTTP.get_response(uri)
+  # end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -18,7 +18,15 @@ RSpec.describe Message, type: :model do
     it "should call the API and obtain the score" do
       message = Message.create(username: 'Hermann', comment: 'Article épatant')
 
-      allow(message).to receive(:moderation).and_return('{"prediction"=>{"0"=>0.022937463372945786}}')
+      allow(message).to receive(:moderation).and_return('{"prediction"=>{"0"=>0.032937463372945786}}')
+      # expect(message).to receive(:callApi).with('https://moderation.logora.fr/')
+    end
+    it "should check if message is accepted" do
+      message1 = Message.create(username: 'Hermann', comment: 'Excellent article!')
+      message2 = Message.create(username: 'Hermann', comment: 'Je déteste ce que vous affirmez, je vais vous tuer!')
+
+      expect(message1[:is_accepted]).to eq(true)
+      expect(message2[:is_accepted]).to eq(false)
     end
   end
 
