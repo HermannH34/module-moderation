83c5e333503d7952b5ef20a614c23da2f66d63c2
Hermann Hairet <hermannhairet44@gmail.com>
2023-01-19 15:57:58 +0100

    connect to moderation API

--- app/models/concerns/moderation.rb
+++ app/models/concerns/moderation.rb
@@ -1,7 +1,13 @@
 module Moderation
   extend ActiveSupport::Concern
 
-  included do
-    validates :comment, length: { in: 3..200 }
-  end
+    included do
+      validates :comment, length: { in: 3..200 }
+    end
+
+    # class_methods do
+    #     debugger
+    #     # response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+    #     "#{:comment}".squish
+    #   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,21 @@
+require "json"
+require "open-uri"
+
 class Message < ApplicationRecord
-  include Moderation
+  # include Moderation
+  validates :comment, length: { in: 3..200 }
   validates :username, presence: true
+
+  before_validation :moderation
+
+  private
+
+  def moderation
+    # self.comment = "hello"
+    url = "https://moderation.logora.fr/"
+    user_serialized = URI.open(url).read
+    user = JSON.parse(user_serialized)
+    debugger
+
+  end
 end
