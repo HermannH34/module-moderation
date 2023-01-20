85e36d03d7132439e248d1ad3bcc4121e50d1756
Hermann Hairet <hermannhairet44@gmail.com>
2023-01-20 09:29:38 +0100

    migrate moderation method ina module

--- app/models/concerns/moderation.rb
+++ app/models/concerns/moderation.rb
@@ -2,12 +2,17 @@ module Moderation
   extend ActiveSupport::Concern
 
     included do
-      validates :comment, length: { in: 3..200 }
+      before_create :moderation
     end
 
-    # class_methods do
-    #     debugger
-    #     # response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
-    #     "#{:comment}".squish
-    #   end
+    def moderation
+      uri = URI('https://moderation.logora.fr/predict')
+      params = { :text => "#{comment}" }
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(uri)
+      # res = callApi(uri)
+      prediction = JSON.parse(res.body)
+
+      prediction["prediction"]["0"].round(2) >= 0.09 ? self.is_accepted = false : self.is_accepted = true
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,24 +3,24 @@ require "open-uri"
 require 'net/http'
 
 class Message < ApplicationRecord
-  # include Moderation
   validates :comment, length: { in: 3..200 }
   validates :username, presence: true
 
-  before_create :moderation
+  include Moderation
+  # before_create :moderation
 
-  private
+  # private
 
-  def moderation
-    uri = URI('https://moderation.logora.fr/predict')
-    params = { :text => "#{comment}" }
-    uri.query = URI.encode_www_form(params)
-    res = Net::HTTP.get_response(uri)
-    # res = callApi(uri)
-    prediction = JSON.parse(res.body)
+  # def moderation
+  #   uri = URI('https://moderation.logora.fr/predict')
+  #   params = { :text => "#{comment}" }
+  #   uri.query = URI.encode_www_form(params)
+  #   res = Net::HTTP.get_response(uri)
+  #   # res = callApi(uri)
+  #   prediction = JSON.parse(res.body)
 
-    prediction["prediction"]["0"].round(2) >= 0.09 ? self.is_accepted = false : self.is_accepted = true
-  end
+  #   prediction["prediction"]["0"].round(2) >= 0.09 ? self.is_accepted = false : self.is_accepted = true
+  # end
 
   # def callApi(uri)
   #   Net::HTTP.get_response(uri)
